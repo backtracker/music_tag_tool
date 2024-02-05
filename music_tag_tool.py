92a2c4c7c5aa40a28f2ad429f671d63f1bb2f1d5
nick.dai <huarong.dai@ai-ym.com>
2024-02-05 10:36:29 +0800

    为了重命名文件时适配单碟修改清洗逻辑，如果专辑是单张碟，将整张专辑碟号字段清空

--- music_tag_tool.py
+++ music_tag_tool.py
@@ -429,7 +429,7 @@ class DsfCleaner(MusicCleaner):
 
 def get_all_music_file() -> List[str]:
     """
-    遍历SRC_DIR目录下全部flac,dsf文件，并返回文件列表
+    遍历SRC_DIR目录下全部flac文件，并返回文件列表
     """
     music_file_list = []
     for root, dirs, files in os.walk(SRC_DIR):
@@ -476,20 +476,53 @@ def delete_empty_dir(folder):
 
 if __name__ == '__main__':
     music_file_list = get_all_music_file()
+    # 根据专辑名称存放歌曲
+    album_music_cleaner_dict = {}
     for music_file in music_file_list:
-        print("开始处理文件: {}".format(music_file))
-
+        print("-"*80)
+        print("开始清洗文件: {}".format(music_file))
         if music_file.endswith(".flac"):
             music_cleaner = FlacCleaner(music_file)
         elif music_file.endswith(".dsf"):
             music_cleaner = DsfCleaner(music_file)
-
         music_cleaner.clean_tags()
+
+        album_music_cleaner_dict.setdefault(music_cleaner.album, []).append(music_cleaner)
+
+    # 开始处理碟号，如果专辑是单张碟，将整张专辑碟号字段清空
+    for album, music_cleaner_list in album_music_cleaner_dict.items():
+        # 专辑是否是单张碟
+        is_album_single_disc = True
+        for music_cleaner in music_cleaner_list:
+            if str(music_cleaner.disc_number).isdigit():
+                if int(music_cleaner.disc_number) > 1:
+                    is_album_single_disc = False
+                    break
+        # 清空单张碟的碟号
+        if is_album_single_disc:
+            for music_cleaner in music_cleaner_list:
+                print("开始清空单碟文件碟号：{}".format(music_cleaner.filename))
+                music_cleaner.disc_number = ""
+
+    print("Tag清洗完成，开始文件处理...")
+    print("=" * 100)
+
+    music_file_list = get_all_music_file()
+    # 排除tag信息不全的文件
+    music_file_list = [music_file for music_file in music_file_list if music_file not in lack_tag_file_list]
+
+    for music_file in music_file_list:
+        print("开始处理文件: {}".format(music_file))
+        if music_file.endswith(".flac"):
+            music_cleaner = FlacCleaner(music_file)
+        elif music_file.endswith(".dsf"):
+            music_cleaner = DsfCleaner(music_file)
         new_file = music_cleaner.rename_file()
 
         if IS_MOVE_FILE_TO_TARGET_DIR and new_file is not None:
             music_cleaner.move_file(new_file)
 
+    # 清空空目录
     if IS_DELETE_SRC_EMPTY_DIR:
         delete_empty_dir(SRC_DIR)
 
